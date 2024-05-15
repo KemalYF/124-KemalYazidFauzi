24b7167c5640b8c95876c05363ff2dc0467245fb
KemalYF <kemalalfauzi@gmail.com>
2024-05-15 22:37:56 +0700

    Update Web Project

--- resources/views/layout/hero.blade.php
+++ resources/views/layout/hero.blade.php
@@ -1,7 +1,16 @@
+<div class="jumbotron">
+  <div class="jumbotron-description">
+      <h1 class="jumbotron-title">Groove Hotel</h1>
+      <h4 class="jumbotron-about">
+          <a href="#room">Rooms & Suits</a>
+      </h4>
+  </div>
+</div>
+
 <main>
-    <div class="content">
-        <div class="content-description">
-          <h1 class="title">Groove Hotel</h1>
+    <div class="hero">
+        <div class="hero-description">
+          <h1 class="hero-title">Groove Hotel</h1>
           <p>
             At Groove Hotel, we believe that every moment you spend with us is an unforgettable story.
             We bring together unparalleled luxury and warm hospitality, creating an environment that
@@ -9,9 +18,9 @@
             savor every moment with extraordinary beauty. With us, your journey is not just about staying
             at a hotel it's an experience that brings happiness and cherished memories to treasure forever.
           </p>
-          <button>What's more</button>
+          <button><a href="#room">What's more</a></button>
         </div>
-        <div class="content-image">
+        <div class="hero-content-image">
             <img src="{{ asset('assets/images/GrooveHotel-2.png') }}" alt="Hotel Logo">
         </div>
       </div>
--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Groove Hotel')
 
 @section('content')
+
 @include('layout.hero')
     {{-- <h1>Welcome to Groove Hotel</h1>
     <p>This is the home page.</p> --}}
